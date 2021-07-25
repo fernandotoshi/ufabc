d41c4b013f51830f6ea03bafa43c5826a2ffd1a8
fernandotoshi <fernando.s.akamine@gmail.com>
2021-07-24 21:22:54 -0300

    Jogo UFABC

---
+++ Fazendinha/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 distCompen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distCompen = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + distCompen;
+    }
+}
---
+++ Fazendinha/Assets/Scripts/PlantController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantController : MonoBehaviour
+{
+    public GameObject plant;
+    public GameObject frameSelected;
+    private bool canPlant;
+
+    private void Start()
+    {
+        canPlant = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && canPlant)
+        {
+            Instantiate(plant, frameSelected.GetComponent<FrameSelectedController>().positionSelected, frameSelected.transform.rotation);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Plant")
+        {
+            canPlant = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Plant")
+        {
+            canPlant = true;
+        }
+    }
+}
